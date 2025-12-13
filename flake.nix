ec15039e563a287713054273a21a31fd3c412dfc
dbeley <6568955+dbeley@users.noreply.github.com>
2025-12-13 21:37:32 +0100

    nix: use comping name in flake.nix, update addonId in manifest

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "bebopa - RYM Cache Overlay browser extension";
+  description = "Comping browser extension";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
